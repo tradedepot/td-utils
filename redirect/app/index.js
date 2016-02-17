37a78f6e4a06e01ed9a82626ea614cd963ead490
Kachi Izukanne <cizukanne@c2gconsulting.com>
2016-02-17 11:26:56 +0100

    strip index.js

--- redirect/app/index.js
+++ redirect/app/index.js
@@ -1,100 +1,25 @@
-var express = require('express'),
-	exphbs = require('express-handlebars'),
-	http = require('http'),
-	routes = require('./routes'),
-  bodyParser = require('body-parser'),
-  defaultConfig = require('./config/default.json');
-
-var logger = require('./lib/log');
-var DDPClient = require("ddp");
+var express = require('express');
+var http = require('http');
 
 
-var ERROR_RESPONSE_CODE = 422;
-var AUTH_ERROR_RESPONSE_CODE = 409;
-  
-// Create an express instance and set a port variable
-var app = exports.app = express();
-var router = express.Router(); // Create our Express router
+//Create server and router
+var app = express();
+var router = express.Router();
 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('views', __dirname + '/views/');
 
-// Create `ExpressHandlebars` instance with a default layout.
-var hbs = exphbs.create({
-    defaultLayout: 'main',
-    layoutsDir: 'views/layouts/',
-    partialsDir: 'views/partials/',
-    compilerOptions: undefined
+app.use(function redirectHTTP(req, res, next) {
+    return res.redirect('https://' + req.headers.host + req.url);
+    next();
 });
 
-// Register `hbs` as our view engine using its bound `engine()` function.
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-
-// Disable etag headers on responses
-app.disable('etag');
-
-// WEB Routes
-app.get('/', routes.index);
-app.get('/signup', routes.signup);
-app.get('/validate/tenant', routes.validateTenant);
-app.use("/", express.static(__dirname + "/public/"));
-
+app.all('/', router); 
 
 //Start server
-var port = 8080;
+var port = process.env.PORT || 80;
 
 var server = http.createServer(app).listen(port, function() {
-  logger.info('TDC server listening on port %d in %s mode', port, app.settings.env);
+  logger.info('Redirect server listening on port %d in %s mode', port, app.settings.env);
 });
 
-// process socket.io
-var io = require('socket.io').listen(server);
-io.on('connection', function(socket) {
-	socket.on('geodata', function(data) {
-		console.log('Geodata: %s', JSON.stringify(data));
-	});
-});
 
-/*
- * Use defaults, assume local connection
- * Deploy as docker image linked to main meteor image
- */
- /*
-var ddpclient = exports.ddpclient = new DDPClient({
-  host : "localhost",
-  port : 3000,
-  ssl  : false,
-  useSockJs : true
-});
-
-/*
- * Connect to the Meteor Server
- *
-ddpclient.connect(function(error, wasReconnect){
-	if (error) {
-		logger.info("DDP connection error: " + error);
-    return;
-	}
-  
-  if (wasReconnect) {
-    logger.info("Connection re-established");
-  }
-  
-  /*
-   * Subscribe to a Meteor Collection
-   *
-  ddpclient.subscribe(
-    'Tenants',                  
-    [],                        
-    function () {              
-      logger.info('Subscribed to tenants');
-      // logger.debug(ddpclient.collections.tenants);
-    }
-  );
-  
-});*/
-  
 
